5c5624d38bec4c4f55f84f5443591e24079ba972
Denis Daletski <daletskidenis@gmail.com>
2022-11-23 00:44:23 +0100

    module macro to define all glsl types; shader struct; some uniforms support

--- shade-rs-examples/src/main.rs
+++ shade-rs-examples/src/main.rs
@@ -1,90 +1,39 @@
-mod glsl {
-    use std::ops::{Add, AddAssign};
+#[shade_rs::module]
+mod shaders {
+    fn call() {}
 
-    type Scalar<T, const N: usize> = [T; N];
+    #[fragment_shader]
+    pub fn Fragment(
+        #[uniform(location = 0)] int_uniform: Int,
+        #[uniform(location = 1)] float_uniform: Float,
+    ) {
+        let mut x: Float4 = float4(1.0, 2.0, 3.0, 4.0);
+        let y: Float4 = float4(0.0, 1.0, 2.0, 3.0);
 
-    #[derive(Debug, Clone, Copy)]
-    pub struct Float4(Scalar<f32, 4>);
-    impl From<Scalar<f32, 4>> for Float4 {
-        fn from(val: Scalar<f32, 4>) -> Self {
-            Self(val)
+        let mut a: Float = sin(10.0);
+
+        x = x + y;
+        x += y;
+
+        for _i in 1..10 {
+            a += float_uniform + int_to_float(int_uniform);
         }
-    }
 
-    pub type Float = f32;
-
-    pub fn float4(x: f32, y: f32, z: f32, w: f32) -> Float4 {
-        [x, y, z, w].into()
-    }
-
-    pub fn sin(val: Float) -> Float {
-        val
-    }
-
-
-    impl AddAssign for Float4 {
-        fn add_assign(&mut self, rhs: Self) {
-            self.0[0] += rhs.0[0];
-            self.0[1] += rhs.0[1];
-            self.0[2] += rhs.0[2];
-            self.0[3] += rhs.0[3];
+        if 1 > 0 {
+            2;
+        } else if false {
+            6;
+        } else {
+            7;
         }
+
+        call();
     }
-
-    impl Add for Float4 {
-        type Output = Self;
-
-        fn add(self, rhs: Self) -> Self::Output {
-            let mut result = self;
-            result += rhs;
-            result
-        }
-    }
-}
-
-use glsl::{float4, sin, Float, Float4};
-
-fn call() {
-}
-
-#[shade_rs::fragment_shader]
-fn fragment() -> &'static str {
-    let a: Float4 = float4(1.0, 2.0, 3.0, 4.0);
-    let mut x: Float4 = float4(0.0, 1.0, 2.0, 3.0);
-    let mut y: Float = sin(10.0);
-
-    x = x + a;
-
-    for i in 1..10 {
-        y += 1.0;
-    }
-
-    if 1 > 0 {
-        2;
-    }
-
-    if 1 > 0 {
-        2;
-    } else {
-        3;
-    }
-
-    if 1 > 0 {
-        2;
-    } else if 3 > 0 {
-        4;
-    } else if 5 > 0 {
-        6;
-    } else {
-        7;
-    }
-
-    call();
-
-    x += x;
 }
 
 fn main() {
-    let code = fragment();
+    let code = shaders::Fragment::code();
     println!("{}", code);
+
+    shaders::Fragment::call(0, 1.0);
 }
--- shade-rs-macros/src/lib.rs
+++ shade-rs-macros/src/lib.rs
@@ -11,25 +11,114 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::visit::Visit;
 use syn::ItemFn;
+use syn::ItemMod;
 
 #[proc_macro_attribute]
 pub fn fragment_shader(_attr: TokenStream, function: TokenStream) -> TokenStream {
-    let function = syn::parse::<ItemFn>(function).unwrap();
+    let mut function = syn::parse::<ItemFn>(function).unwrap();
 
     let fun_name = format_ident!("{}", function.sig.ident.to_string());
-
-    let mut visitor = ShaderFnParser::new();
-    visitor.visit_item_fn(&function);
-    let code = visitor.code;
-
     let block = &function.block;
 
-    let expanded = quote! {
-        fn #fun_name() -> &'static str {
-            fn _orig_func_just_for_compiler_checks()
-            #block
+    let mut code_parser = ShaderFnParser::new();
+    code_parser.visit_item_fn(&function);
+    let code = code_parser.code;
 
-            #code
+    // TODO: process uniforms
+    for arg in &mut function.sig.inputs {
+        let syn::FnArg::Typed(arg) = arg else {
+            panic!();
+        };
+
+        arg.attrs.clear();
+    }
+
+    let args = function.sig.inputs;
+
+    let expanded = quote! {
+        pub struct #fun_name {
+        }
+
+        impl #fun_name {
+            pub fn call(#args) {
+                #block
+            }
+
+
+            pub fn code() -> &'static str {
+                #code
+            }
+
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_attribute]
+pub fn module(_attr: TokenStream, program: TokenStream) -> TokenStream {
+    let module = syn::parse::<ItemMod>(program).unwrap();
+
+    let mod_name = format_ident!("{}", module.ident.to_string());
+    let mod_items = module.content.map(|(_, items)| items).unwrap_or(vec![]);
+
+    let expanded = quote! {
+        mod #mod_name {
+            use std::ops::{Add, AddAssign};
+            use shade_rs::fragment_shader;
+
+            type Scalar<T, const N: usize> = [T; N];
+
+            #[derive(Debug, Clone, Copy)]
+            pub struct Float4(Scalar<f32, 4>);
+            impl From<Scalar<f32, 4>> for Float4 {
+                fn from(val: Scalar<f32, 4>) -> Self {
+                    Self(val)
+                }
+            }
+
+            pub type Float = f32;
+            pub type Int = i32;
+
+            // TODO: better cast alternative
+            // TODO: generate this kind of functions
+            fn int_to_float(i: Int) -> Float {
+                i as Float
+            }
+
+            pub fn float4(x: f32, y: f32, z: f32, w: f32) -> Float4 {
+                [x, y, z, w].into()
+            }
+
+            pub fn float(x: f32) -> Float {
+                x
+            }
+
+
+            pub fn sin(val: Float) -> Float {
+                val
+            }
+
+            impl AddAssign for Float4 {
+                fn add_assign(&mut self, rhs: Self) {
+                    self.0[0] += rhs.0[0];
+                    self.0[1] += rhs.0[1];
+                    self.0[2] += rhs.0[2];
+                    self.0[3] += rhs.0[3];
+                }
+            }
+
+            impl Add for Float4 {
+                type Output = Self;
+
+                fn add(self, rhs: Self) -> Self::Output {
+                    let mut result = self;
+                    result += rhs;
+                    result
+                }
+            }
+
+            #(#mod_items)*
         }
     };
 
